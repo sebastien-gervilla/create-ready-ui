cfe50fea60303447857a1e3e0c276c35d263e87e
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-01-13 09:14:13 +0100

    Added --vue option

--- bin/index.js
+++ bin/index.js
@@ -6,10 +6,25 @@ import { generateTemplate } from '../utils/generate.js';
 import { exitProcces } from '../utils/exit-process.js';
 import messages from '../docs/messages.json' assert { type: "json" };
 
+const targets = {
+    react: {
+        url: 'https://github.com/sebastien-gervilla/skz-ready-ui',
+        rootFolder: 'skz-ready-ui-master'
+    },
+    vue: {
+        url: 'https://github.com/sebastien-gervilla/vue-ready-ui',
+        rootFolder: 'vue-ready-ui-master'
+    }
+};
+
 (async () => {
     const projectName = process.argv[2];
     if (!projectName) exitProcces(1, [messages.fail.missingName])
 
-    const projectDirectory = path.resolve(process.cwd(), projectName);
+    let target = targets.react;
+    if (process.argv[3] === '--vue')
+        target = targets.vue;
+
+    const projectDirectory = path.resolve(process.cwd(), projectName, target);
     await generateTemplate(projectName, projectDirectory);
 })();
